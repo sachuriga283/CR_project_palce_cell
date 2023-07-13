394c6bbdf1c1a92ff892f3dd7aef61e2b27b3fc1
Sachuriga <sachuriga3@gmail.com>
2023-07-13 14:13:25 +0200

    1.01

--- data/spike_position_filtered_with_speed.m
+++ data/spike_position_filtered_with_speed.m
@@ -1,7 +1,10 @@
-function [outputArg1,outputArg2] = untitled(inputArg1,inputArg2)
+function [outputArg1,outputArg2] = spike_position_filtered_with_speed(activity)
+
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 outputArg1 = inputArg1;
 outputArg2 = inputArg2;
+
+
 end
 
